21aeed83850ca0a21a98ff5ab28f36511f7b40ac
Maha1805 <mahalakshmi.pattan@nutechnologyinc.com>
2022-07-25 19:35:04 +0530

    Spring Boot Profile task

---
+++ ProfileDemo/src/main/java/com/springboot/profiles/controller/ProfileController.java
@@ -0,0 +1,19 @@
+package com.springboot.profiles.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class ProfileController {
+
+	@Value("${spring.profile.message}")
+	private String msg;
+	
+	@GetMapping(value="/profiles")
+	public String display()
+	{
+		return msg;
+	}
+}
